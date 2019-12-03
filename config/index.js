6c33259a252d0b02c03b9c281144af7b314ff31a
Jari-Pekka Ryynänen <jappi85@gmail.com>
2019-12-03 21:12:07 +0200

    Fix public path

--- config/index.js
+++ config/index.js
@@ -50,7 +50,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: 'linnanjuhlat-bingo',
+    assetsPublicPath: '/linnanjuhlat-bingo',
 
     /**
      * Source Maps
